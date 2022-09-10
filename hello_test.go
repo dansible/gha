ac775ba09c771ad3cf5a2bb00ef74f963a1ccd95
Daniel Pacrami <danielpacrami@gmail.com>
2022-09-10 12:32:46 -0400

    Update hello_test.go

--- hello_test.go
+++ hello_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	got := Hello()
-	want := "Continuous Delivery is awesome!"
+	want := "Continuous Integration is awesome!"
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
